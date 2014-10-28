32dc55dbcd8798cd7c6c86c012582d744122f269
Alejandro Pulver <alejandro@8thlight.com>
2014-10-28 16:30:29 -0300

    Add results scripts

--- myapp-results/README.md
+++ myapp-results/README.md
@@ -1,10 +1,13 @@
 # Results analysis
 
+The subjects' data can be accessed through the appropriate URL, but as it's not always fetched properly I recommend using `links`. Otherwise the database dump can be obtained with [db-tools](https://github.com/meteor-london/db-tools) in BJSON (Binary JSON) and converted to regular (plain text) JSON.
+
+A script for analysis of the quantity and "completeness" of experiments is available as `generate_stats.py`. To get the data as a CSV, use `generate_results.py`. 
+
 ...
 
-#### Import and export data
+# Links
 
-- https://github.com/meteor-london/db-tools
 - [Export from server](https://gist.github.com/olizilla/5209369)
 - [Import from server](https://gist.github.com/IslamMagdy/5519514)
-- Delete remote database: `meteor deploy ... --delete`
+- [neuro-circles-json-to-csv](https://github.com/chudichudichudi/neuro-circles-json-to-csv)
---
+++ myapp-results/generate_stats.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+from collections import defaultdict
+import csv
+
+
+class Stage:
+    def __init__(self, data, experiment):
+        self._data = data
+        self._experiment = experiment
+
+    @staticmethod
+    def row_header():
+        return ['exp_id', 'size', 'start_time', 'end_time', 'name', 'complete']
+
+    def size_in_bytes(self):
+        return len(json.dumps(self._data))
+
+    def row(self):
+        return [
+            self._data['experiment'],
+            self.size_in_bytes(),
+            self._data['start_time'],
+            self._data['end_time'],
+            self._data['stage'],
+            self._experiment.is_complete()
+        ]
+
+
+class Experiment:
+    def __init__(self, data):
+        self._data = data
+
+    def get_stage(self, name):
+        return next(filter(lambda s: s['stage'] == name, self._data))
+
+    @staticmethod
+    def row_header():
+        return [
+            'exp_id', 'size', 'num_stages', 'start_time',
+            'end_time', 'participant', 'group',
+            'ip_address', 'user_agent', 'local_id', 'complete'
+        ]
+
+    def size_in_bytes(self):
+        return len(json.dumps(self._data))
+
+    def end_time(self):
+        end_time = max(map(lambda s: s['end_time'], self._data))
+        return end_time
+
+    def is_complete(self):
+        return len(self._data) == 8
+
+    def stages(self):
+        return (Stage(s, self) for s in self._data)
+
+    def row(self):
+        introduction = self.get_stage("introduction")
+        results = introduction['results']
+
+        return [
+            self._data[0]['experiment'],
+            self.size_in_bytes(),
+            len(self._data),
+            introduction['start_time'],
+            self.end_time(),
+            results['participant'],
+            results['group'],
+            results['ip_address'],
+            results['user_agent'],
+            results['local_id'] if 'local_id' in results else 'unknown',
+            self.is_complete()
+        ]
+
+
+def write_csv(header, rows, file_name):
+    with open(file_name, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        writer.writerow(header)
+        for r in rows:
+            writer.writerow(r.row())
+
+
+def main(argv):
+    with open(argv[1], "r") as fp:
+        stages = json.load(fp)
+    # XXX: remove old versions with incomplete data
+    condition = lambda s: ('start_time' in s.keys()) and s['start_time'] > 1409788800000
+    stages = list(filter(condition, stages))
+    print("%s stages" % len(stages))
+
+    experiments = defaultdict(list)
+    for s in stages:
+        experiments[s['experiment']].append(s)
+    print("%s experiments" % len(experiments))
+
+    experiment_objs = [Experiment(e) for e in experiments.values()]
+    stage_objs = [s for e in experiment_objs for s in e.stages()]
+
+    write_csv(Experiment.row_header(), experiment_objs, 'experiments.csv')
+    write_csv(Stage.row_header(), stage_objs, 'stages.csv')
+
+    return 0
+
+
+if __name__ == "__main__":
+    status = main(sys.argv)
+    sys.exit(status)
